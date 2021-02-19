944fde642cb83124b30ee240e12493bfd8c179a2
Mooose <themooose@gmail.com>
2021-02-19 08:05:14 -0600

    bad idea

--- zsh/apply-zsh.sh
+++ zsh/apply-zsh.sh
@@ -4,21 +4,21 @@
 # ensure that zsh and oh-my-zsh are installed
 
 # Be sure to run this from the mooose-pub/zsh directory!
-[ ! -f apply-zsh.sh ] && { echo "Be sure you are running this from the mooose-pub/zsh directory."; exit; }
+# [ ! -f apply-zsh.sh ] && { echo "Be sure you are running this from the mooose-pub/zsh directory."; exit; }
 
-# pull the p10k theme into omz
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# # pull the p10k theme into omz
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# update this copy of .zshrc to reference the current user
-sed -i "s/mooose/${USER}/g" .zshrc
+# # update this copy of .zshrc to reference the current user
+# sed -i "s/mooose/${USER}/g" .zshrc
 
-# backup local .zshrc (if necessary) and replace it
-[ -f ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.bak
-cp .zshrc ~/.zshrc
+# # backup local .zshrc (if necessary) and replace it
+# [ -f ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.bak
+# cp .zshrc ~/.zshrc
 
-# backup local .zshrc (if necessary) and replace it
-[ -f ~/.p10k.zsh ] && mv ~/.p10k.zsh ~/.p10k.zsh.bak
-cp .p10k.zsh ~/.p10k.zsh
+# # backup local .zshrc (if necessary) and replace it
+# [ -f ~/.p10k.zsh ] && mv ~/.p10k.zsh ~/.p10k.zsh.bak
+# cp .p10k.zsh ~/.p10k.zsh
 
-. ~/.zshrc
-echo "zsh config applied. Happy coding!"
+# . ~/.zshrc
+echo "zsh config applied. Happy coding!"sd
